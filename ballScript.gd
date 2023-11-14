9da31b4f8d65884b20be6b8c5c58e9aca7525642
Khillo <khalilwarwar2@gmail.com>
2023-11-14 21:01:16 +0200

    Working ball

---
+++ ballScript.gd
@@ -0,0 +1,55 @@
+extends RigidBody2D
+
+const areaSize = 5
+const growSize = 0.02
+const imageName = 'res://icons/Sushi'
+const imageFormat = '.svg'
+const numberOfImages = 6
+
+var alive = true
+var level = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	_update_area_collision()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+func _grow():
+	if(level < numberOfImages - 1):
+		level += 1
+		get_node("CollisionShape2D/Sprite2D").texture = load(imageName + str(level) + imageFormat)
+		get_node("CollisionShape2D").scale += Vector2.ONE * growSize
+		_update_area_collision()
+		mass += 1
+	else:
+		level += 0
+		get_node("CollisionShape2D/Sprite2D").texture = load(imageName + str(level) + imageFormat)
+		get_node("CollisionShape2D").scale += Vector2.ONE * (-growSize) * (numberOfImages - 1)
+		_update_area_collision()
+		mass += 1
+	
+func _update_area_collision():
+	var areaCollision = get_node("Area2D/CollisionShape2D")
+	var sprite = get_node("CollisionShape2D/Sprite2D")
+	var spriteCollision = get_node("CollisionShape2D")
+	
+	var newShape = CircleShape2D.new()
+	print_debug(sprite.texture.get_width())
+	print_debug(sprite.scale.y)
+	newShape.radius = sprite.texture.get_width() * spriteCollision.scale.y / 2 + areaSize
+	areaCollision.shape = newShape
+
+func _kill():
+	alive = false
+	get_node("CollisionShape2D").disabled = true
+	queue_free()
+
+func _on_area_2d_area_entered(area):
+	var other = area.get_owner()
+	if(alive and other.get_script().get_path() == "res://ballScript.gd"):
+		other._kill()
+		_grow()
